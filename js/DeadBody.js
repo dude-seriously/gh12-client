4a3c6cb341fb88f74c568c3423ebc4542177421a
Maciek Baron <thebezet@gmail.com>
2012-04-30 12:21:46 +0200

    Comments

--- js/DeadBody.js
+++ js/DeadBody.js
@@ -1,11 +1,17 @@
+/* 
+	YES THIS IS A DEAD BODY CLASS
+	
+	DEAL WITH IT
+	
+*/
 function DeadBody(x, y, evil) {
 	this.x = x;
 	this.y = y;
-	this.evil = evil;
+	this.evil = evil; // A body can not only be dead, but also evil!
 
-	this.visible = true;
+	this.visible = true; // Draw me after creation
 
-	deadBodies[deadBodies.length] = this;
-	var self = this;
-	setTimeout(function() {self.visible = false;}, 5000);
+	deadBodies[deadBodies.length] = this; // Add to the end of the array
+	var self = this; // Point at myself
+	setTimeout(function() {self.visible = false;}, 5000); // Quick timeout hack to remove body
 }
